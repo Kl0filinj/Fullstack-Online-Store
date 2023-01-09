8657a0258eea28fec4ed235098148b61330db3bb
Velychko Daniil <101584452+Kl0filinj@users.noreply.github.com>
2023-01-09 17:40:22 +0100

    home styles | layout

--- src/frontEnd/components/Layout.jsx
+++ src/frontEnd/components/Layout.jsx
@@ -1,9 +1,47 @@
 import { Outlet } from 'react-router-dom';
+import {
+  Flex,
+  Spacer,
+  Box,
+  Heading,
+  Button,
+  IconButton,
+  useColorMode,
+} from '@chakra-ui/react';
+import { SunIcon } from '@chakra-ui/icons';
 
 const Layout = () => {
+  const { toggleColorMode } = useColorMode();
   return (
     <>
-      <Outlet />
+      <Box px="4">
+        <Flex px="6" py="5" maxW="6xl" mx="auto">
+          <Box>
+            <Heading
+              as="span"
+              color="red.700"
+              fontWeight="700"
+              fontFamily="Libre Baskerville"
+            >
+              MeelDeel
+            </Heading>
+          </Box>
+          <Spacer />
+          <Box>
+            <IconButton
+              variant="ghost"
+              onClick={toggleColorMode}
+              icon={<SunIcon />}
+            ></IconButton>
+          </Box>
+          <Box>
+            <Button colorScheme="red" variant="outline">
+              Sign In
+            </Button>
+          </Box>
+        </Flex>
+        <Outlet />
+      </Box>
     </>
   );
 };
